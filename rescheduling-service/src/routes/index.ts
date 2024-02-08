75d6cef614481b4bafd3d60e57bd7a90f2e7fe2a
Deepu Premrajan <deepu.premrajan@gmail.com>
2024-02-07 23:42:27 -0800

    added create-offer route and super rudimentary logic

--- rescheduling-service/src/routes/index.ts
+++ rescheduling-service/src/routes/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import availableAppointmentsRouter from './availableAppointments';
 import availablePatientsRouter from './availablePatients';
+import createOfferRouter from './createOffer';
 
 const router = express.Router();
 
 router.use('/available-appointments', availableAppointmentsRouter);
 router.use('/patients', availablePatientsRouter);
+router.use('/create-offer', createOfferRouter);
 
 export default router;
+
+// lamar should take appoint 197 in offers
