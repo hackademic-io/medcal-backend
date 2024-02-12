83e9a6c5468d1b39e9203c9b488958fd6c1f139f
Deepu Premrajan <deepu.premrajan@gmail.com>
2024-02-12 12:51:30 -0800

    refactored available appointments route to use appointment controller

---
+++ rescheduling-service/src/services/db-service/AppointmentRepository.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export class AppointmentRepository {
+  async fetchAvailableAppointments() {
+    try {
+      const availableAppointmentsWithOffers = await prisma.appointment.findMany(
+        {
+          where: { booked: false },
+          include: { offer: true },
+          orderBy: { date: 'asc' },
+        }
+      );
+
+      const availableAppointments = availableAppointmentsWithOffers.filter(
+        (appointment) => !appointment.offer
+      );
+      return availableAppointments;
+    } catch (error) {
+      console.error('Error fetching available appointments', error);
+      throw new Error('Unable to fetch available appointments');
+    }
+  }
+}
+
+export default new AppointmentRepository();
