1236617b2821c5172fc65e1d10995938a291b507
Deepu Premrajan <deepu.premrajan@gmail.com>
2024-02-13 13:25:28 -0800

    fixed repetitive instances

--- rescheduling-service/src/controllers/appointment-controller.ts
+++ rescheduling-service/src/controllers/appointment-controller.ts
@@ -1,13 +1,11 @@
 import { Request, Response } from 'express';
-import { AppointmentRepository } from '../services/db-service/AppointmentRepository';
+import appointmentRepository from '../services/db-service/AppointmentRepository';
 
 export class AppointmentController {
-  private appointmentRepository = new AppointmentRepository();
-
   listAvailableAppointments = async (req: Request, res: Response) => {
     try {
       const availableAppointments =
-        await this.appointmentRepository.fetchAvailableAppointments();
+        await appointmentRepository.fetchAvailableAppointments();
       res.json(availableAppointments);
     } catch (error) {
       res.status(500).json({ message: error.message });
--- rescheduling-service/src/controllers/patient-controller.ts
+++ rescheduling-service/src/controllers/patient-controller.ts
@@ -1,13 +1,11 @@
 import { Request, Response } from 'express';
-import { PatientRepository } from '../services/db-service/PatientRepository';
+import patientRepository from '../services/db-service/PatientRepository';
 
 export class PatientController {
-  private patientRepository = new PatientRepository();
-
   listAvailablePatients = async (req: Request, res: Response) => {
     try {
       const availablePatients =
-        await this.patientRepository.fetchAvailablePatients();
+        await patientRepository.fetchAvailablePatients();
       res.json(availablePatients);
     } catch (error) {
       res.status(500).json({ message: error.message });
