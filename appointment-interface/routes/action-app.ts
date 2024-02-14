470b0e073a92b1da8db0d9d805ff2636ec85f014
Mykhailo Fomenko <misha.fomenko00@gmail.com>
2024-02-14 17:59:58 -0500

    changing appointment-interface to ts

--- appointment-interface/routes/action-app.ts
+++ appointment-interface/routes/action-app.ts
@@ -1,17 +1,17 @@
-const express = require('express')
+import express, { Response, Request } from 'express'
 const sendMessageToInitAppExc = require('../models/init_app_exc_producer')
 
 const actionAppRoutes = express.Router()
 
-actionAppRoutes.post('/appointment', (req, res) => {
+actionAppRoutes.post('/appointment', (req: Request, res: Response) => {
     const { msg, router_key } = req.body
 
     sendMessageToInitAppExc({ msg, router_key }).then(() => {
         res.status(200).send('Message successfully sent to the exchange')
-    }).catch(error => {
+    }).catch((error: Error) => {
         console.error('Failed to send message to RabbitMQ:', error);
         res.status(500).send('Failed to send message');
     })
 })
 
-module.exports = actionAppRoutes
+export default actionAppRoutes
