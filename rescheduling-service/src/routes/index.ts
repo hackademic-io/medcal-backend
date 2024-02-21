24f5bbf0a62eed4a8ba0c15281465844be1e6198
Deepu Premrajan <deepu.premrajan@gmail.com>
2024-02-21 13:26:05 -0800

    deleted unnecessary code related to patient, appointment, offer, and repositories

--- rescheduling-service/src/routes/index.ts
+++ rescheduling-service/src/routes/index.ts
@@ -1,18 +1,5 @@
 import express from 'express';
-import { AppointmentController } from '../controllers/appointment-controller';
-import { PatientController } from '../controllers/patient-controller';
-import { OfferController } from '../controllers/offer-controller';
 
 const router = express.Router();
-const patientController = new PatientController();
-const appointmentController = new AppointmentController();
-const offerController = new OfferController();
-
-router.get(
-  '/available-appointments',
-  appointmentController.listAvailableAppointments
-);
-router.get('/opted-in', patientController.listAvailablePatients);
-router.post('/create-offer', offerController.createOffer);
 
 export default router;
