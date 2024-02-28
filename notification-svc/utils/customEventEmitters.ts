6731201f11f52334db8dba6bf491caacb3c070a9
Mykhailo Fomenko <misha.fomenko00@gmail.com>
2024-02-27 21:31:57 -0500

    moved deleting ignored appointments to appointment-svc

--- notification-svc/utils/customEventEmitters.ts
+++
@@ -1,5 +0,0 @@
-import EventEmitter from 'events'
-class ReschedulingPromtEmitter extends EventEmitter { }
-export const reschedulingEventEmitter = new ReschedulingPromtEmitter()
-class ReminderEmitter extends EventEmitter { }
-export const reminderEmitter = new ReminderEmitter
