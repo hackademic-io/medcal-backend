93467aa135cba3087787a51596a9a9a34b509c6d
Deepu Premrajan <deepu.premrajan@gmail.com>
2024-03-04 21:12:18 -0800

    uncommented cron

--- cron/index.ts
+++ cron/index.ts
@@ -2,5 +2,4 @@ import { fetchAndPublishAppointments } from "./jobs/fetchAndPublishAppointments"
 import cron from "node-cron";
 import "dotenv/config";
 
-// cron.schedule("0 17 * * *", fetchAndPublishAppointments);
-fetchAndPublishAppointments();
+cron.schedule("0 17 * * *", fetchAndPublishAppointments);
